b21e5dfb8f3664e7732dca751d24009c5c8645e9
Marcin Gryszko <marcin@gryszko.net>
2011-05-18 21:09:18 +0200

    Plugin descriptor updated.

--- EqualsHashcodeTestGrailsPlugin.groovy
+++ EqualsHashcodeTestGrailsPlugin.groovy
@@ -3,10 +3,11 @@ class EqualsHashcodeTestGrailsPlugin {
     def grailsVersion = '1.3 > *'
     def dependsOn = [spock: '0.5-groovy-1.7']
 
-    def author = "Marcin Gryszko"
-    def authorEmail = 'marcin.gryszko@osoco.es'
-    def title = "Base Spock specification for equals and hashCode tests"
+    def author = 'Marcin Gryszko'
+    def authorEmail = 'marcin@gryszko.net'
+    def title = 'Base Spock specification for equals and hashCode tests'
     def description = '''\\
-Base Spock specification for testing equals and hashCode methods of domain classes.
+Base Spock specification for testing equals and hashCode methods of Grails domain classes and other Groovy objects.
 '''
+    def documentation = 'https://github.com/osoco/grails-equals-hashcode/wiki'
 }
