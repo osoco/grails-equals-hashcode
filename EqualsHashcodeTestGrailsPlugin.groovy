11b96188229c82ed81368bcab0d25ace89d4fc9d
Marcin Gryszko <marcin@grysz.com>
2013-01-03 17:30:42 +0100

    Updated to Spock 0.7 and removed an explicit dependency to Spock (should be configured by the plugin client)

--- EqualsHashcodeTestGrailsPlugin.groovy
+++ EqualsHashcodeTestGrailsPlugin.groovy
@@ -1,13 +1,20 @@
 class EqualsHashcodeTestGrailsPlugin {
-    def version = '0.2-spock-0.6'
-    def grailsVersion = '2.0.0 > *'
-    def dependsOn = [spock: '0.6']
+    def version = '0.3'
+    def grailsVersion = '1.3.7 > *'
 
-    def author = 'Marcin Gryszko'
-    def authorEmail = 'marcin@grysz.com'
     def title = 'Base Spock specification for equals and hashCode tests'
     def description = '''\\
 Base Spock specification for testing equals and hashCode methods of Grails domain classes and other Groovy objects.
 '''
+
+    def author = 'Marcin Gryszko'
+    def authorEmail = 'marcin.gryszko@osoco.es'
+    def developers = [
+        [name: 'Marcin Gryszko', email: 'marcin.gryszko@osoco.es'],
+    ]
+
+    def license = 'APACHE'
     def documentation = 'http://osoco.github.com/grails-equals-hashcode/'
+    def scm = [url: 'https://github.com/osoco/grails-equals-hashcode']
+    def issueManagement = [system: 'GitHub', url: 'https://github.com/osoco/grails-equals-hashcode/issues']
 }
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,17 +1,26 @@
 grails.release.scm.enabled = false
 grails.project.docs.output.dir = "docs"
-grails.project.dependency.resolution = {
-    inherits("global") {
-        excludes 'ehcache'
-    }
-    repositories {
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
 
+grails.project.dependency.resolution = {
+    inherits 'global'
+    log 'warn'
+
+    repositories {
+        grailsCentral()
         mavenCentral()
     }
+
     dependencies {
         test 'commons-lang:commons-lang:2.6'
+    }
+
+    plugins {
+        test ":spock:0.7", {
+            export = false
+        }
+
+        build(':release:2.2.0', ':rest-client-builder:1.0.3') {
+            export = false
+        }
     }
 }
